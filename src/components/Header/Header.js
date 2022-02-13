59a9f2c03905458bb565570bda76e7eee8ccf526
Demigod21 <davide.falcone276@gmail.com>
2022-02-13 10:05:59 +0100

    added header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,45 @@
 import Link from 'next/link';
 import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+import { DiTerminal } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Span, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a style={{ display : "flex", alignItems : "center", color : 'white', marginBottom : '20px'}}>
+          <DiTerminal size = "3rem" /> <Span>Portfolio</Span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About me</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/Demigod21">
+        <AiFillGithub size = "3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.linkedin.com/in/davide-falcone/">
+        <AiFillLinkedin size = "3rem" />
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
